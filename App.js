9824ad9dbd1d120573f16dcb58cdf7b8deef1716
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-08-10 11:15:47 +0100

    Add buttons on main screen

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Good morning!</Text>
+      <ThemeProvider>
+        <Button titleStyle={{ color: 'magenta' }} type='clear' title="Breath" />
+        <Button type='clear' title="Affirmations" />
+        <Button titleStyle={{ color: 'magenta' }} type='clear' title="Intentions" />
+        <Button type='clear' title="Gratitude" />
+        <Button titleStyle={{ color: 'magenta' }} type='clear' title="Exercise" />
+      </ThemeProvider>
       <StatusBar style="auto" />
     </View>
   );
