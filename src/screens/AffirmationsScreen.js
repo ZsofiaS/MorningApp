2af12610a0a92e853bcc2195d115faed7a7e7ea1
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-08-14 14:19:43 +0100

    Add affirmation feature - fetching

--- src/screens/AffirmationsScreen.js
+++ src/screens/AffirmationsScreen.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import colours from '../../constants/colours.js';
 
-export default function AffirmationsScreen() {
-  return (
-    <View>
-      <Text> Here are some affirmations</Text>
-    </View>
-  )
+export default class AffirmationsScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      affirmation: null
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('https://www.affirmations.dev')
+      .then(response => response.json())
+      .then((responseJson) => {
+        this.setState({
+          affirmation: responseJson["affirmation"]
+        })
+      })
+  };
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.content}>{this.state.affirmation == null ? '' : `"${this.state.affirmation}"`}</Text>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    color: colours.primaryColour,
+    fontSize: 25,
+    marginVertical: 140,
+    paddingHorizontal: 50,
+    textAlign: 'center'
+  }
+})
